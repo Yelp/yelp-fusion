51522704ce3c53191e0daa7421a180165ad0cfed
Tony Liu <tony@zutanolabs.com>
2017-12-19 00:59:52 -0800

    updated sample to use API key

--- fusion/node/sample.js
+++ fusion/node/sample.js
@@ -2,24 +2,21 @@
 
 const yelp = require('yelp-fusion');
 
-// Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+// Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const clientId = '<YOUR_CLIENT_ID>';
-const clientSecret = '<YOUR_CLIENT_SECRET>';
+const apiKey = '<YOUR_API_KEY>';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  term:'zoo',
+  location: 'california'
 };
 
-yelp.accessToken(clientId, clientSecret).then(response => {
-  const client = yelp.client(response.jsonBody.access_token);
+const client = yelp.client(apiKey);
 
-  client.search(searchRequest).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    console.log(prettyJson);
-  });
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses[0];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
 }).catch(e => {
   console.log(e);
 });
